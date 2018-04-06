d97feb466b97546a115cf1a4221771624b3a4630
niuyueheng <niuyueheng@jd.com>
2018-04-07 03:24:08 +0800

    add .gitignore

---
+++ yanyi-dictionary-common/src/main/java/com/yanyi/common/model/UserInfo.java
@@ -0,0 +1,99 @@
+package com.yanyi.common.model;
+
+import java.util.Date;
+
+/**
+ *  用户信息类
+ */
+public class UserInfo {
+
+    private int id;
+
+    private String nickName;
+
+    private String gender;
+
+    private String avatarUrl;
+
+    private String city;
+
+    private String province;
+
+    private String country;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
