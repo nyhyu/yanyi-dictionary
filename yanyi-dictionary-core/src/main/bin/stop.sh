d40f21cb450ea14e5d3244e0650ed7864ecee2c4
niuyueheng <niuyueheng@jd.com>
2018-04-10 00:52:37 +0800

    修改stop脚本

--- yanyi-dictionary-core/src/main/bin/stop.sh
+++ yanyi-dictionary-core/src/main/bin/stop.sh
@@ -6,7 +6,7 @@ readonly BASE_LOG_DIR="/export/Logs/yanyi-dictionary"
 CRON_ENTRY="*/1 * * * * ${YANYI_DICTIONARY_HOME}/bin/start.sh >> $BASE_LOG_DIR/console.log > /dev/null 2>&1"
 CRON_ENTRY_ESCAPE=${CRON_ENTRY//\*/\\*}  # Escape all '*'
 readonly YANYI_DICTIONARY_PROC="com.yanyi.core.Application"
-pid=`jps -l | grep YANYI_DICTIONARY_PROC | awk '{print $1}'`
+pid=`jps -l | grep $YANYI_DICTIONARY_PROC | awk '{print $1}'`
 
 if [[ $pid != "" ]] ;then
     kill -9 $pid
